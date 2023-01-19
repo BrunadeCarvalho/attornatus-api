cb0a6bfc371e340c86f013a6b6003be1423a03dd
Bruna de Carvalho <brunacarvalho260697@gmail.com>
2023-01-19 12:52:55 -0300

    FEAT: Segundo endpoint criado: Listar todos os clientes cadastrados.

--- src/main/java/me/teste/attornatus/resource/ClientResource.java
+++ src/main/java/me/teste/attornatus/resource/ClientResource.java
@@ -2,20 +2,29 @@ package me.teste.attornatus.resource;
 
 import lombok.RequiredArgsConstructor;
 import me.teste.attornatus.models.Client;
+import me.teste.attornatus.repository.ClientRepository;
 import me.teste.attornatus.service.ClientService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/client")
 @RequiredArgsConstructor
 public class ClientResource {
     private final ClientService clientService;
+    private final ClientRepository clientRepository;
 
     @GetMapping("/{id}")
     public Client getSacola(@PathVariable("id") Long id){
         return clientService.getClient(id);
+    }
+
+    @GetMapping()
+    public List<Client> getAllClients(){
+        return clientRepository.findAll();
     }
 }
--- src/main/java/me/teste/attornatus/service/ClientServiceImpl.java
+++ src/main/java/me/teste/attornatus/service/ClientServiceImpl.java
@@ -17,4 +17,7 @@ public class ClientServiceImpl implements ClientService{
                 }
         );
     }
+
+
+
 }
